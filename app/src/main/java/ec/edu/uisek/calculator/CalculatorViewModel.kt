85a5935e0e810bbbc2af338383de0eb2cb08e37a
Nelson Guijarro <nrguijarro@puce.edu.ec>
2025-10-28 15:38:59 -0500

    Laboratorio 2 Parte 2

--- app/src/main/java/ec/edu/uisek/calculator/CalculatorViewModel.kt
+++ app/src/main/java/ec/edu/uisek/calculator/CalculatorViewModel.kt
@@ -3,13 +3,14 @@ package ec.edu.uisek.calculator
 import android.view.Display
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 data class CalculatorState(
     val display: String = "0"
 )
 
-sealed class CalculatorEvent{
+sealed class CalculatorEvent {
     data class Number(val number: String) : CalculatorEvent()
     data class Operator(val operator: String) : CalculatorEvent()
     object Clear : CalculatorEvent()
@@ -18,15 +19,16 @@ sealed class CalculatorEvent{
     object Decimal : CalculatorEvent()
 }
 
-class CalculatorViewModel : ViewModel (){
+class CalculatorViewModel : ViewModel() {
     private var number1: String = ""
     private var number2: String = ""
     private var operator: String? = null
 
     var state by mutableStateOf(CalculatorState())
         private set
-    fun onEvent(event: CalculatorEvent){
-        when (event){
+
+    fun onEvent(event: CalculatorEvent) {
+        when (event) {
             is CalculatorEvent.Number -> enterNumber(event.number)
             is CalculatorEvent.Operator -> enterOperator(event.operator)
             is CalculatorEvent.Decimal -> enterDecimal()
@@ -45,10 +47,11 @@ class CalculatorViewModel : ViewModel (){
             (operator == null)
             number1 += number
             state = state.copy(display = number1)
-      }
+        }
     }
+
     private fun enterOperator(operator: String) {
-        if (number1.isNotBlank()){
+        if (number1.isNotBlank()) {
             this.operator = operator
         }
     }
@@ -57,23 +60,72 @@ class CalculatorViewModel : ViewModel (){
         val currentNumber = if (operator == null) number1 else number2
         if (!currentNumber.contains(".")) {
             if (operator == null) {
-                number1 += "."}
-                state = state.copy(display = number1)
-            } else {
-                number2 += "."
-                state = state.copy(display = number2)
+                number1 += "."
+            }
+            state = state.copy(display = number1)
+        } else {
+            number2 += "."
+            state = state.copy(display = number2)
         }
     }
 
     private fun clearLast() {
-        TODO("Not yet implemented")
+        if (operator == null) {
+            if (number1.isNotBlank()) {
+                number1 = number1.dropLast(1)
+                state = state.copy(
+                    if (number1.isBlank())
+                        "0"
+                    else
+                        number1
+                )
+            } else {
+                if (number2.isNotBlank()) {
+                    number2 = number2.dropLast(1)
+                    state = state.copy(
+                        if (number2.isBlank())
+                            "0"
+                        else
+                            number2
+                    )
+                } else {
+                    operator = null
+                    state = state.copy(number1)
+                }
+            }
+        }
     }
 
     private fun clearAll() {
-        TODO("Not yet implemented")
+        number1 = ""
+        number2 = ""
+        operator = null
+        state = state.copy("0")
     }
 
     private fun performCalculation() {
-        TODO("Not yet implemented")
+        val num1 = number1.toDoubleOrNull()
+        val num2 = number2.toDoubleOrNull()
+        if (num1 != null && num2 != null && operator != null) {
+            val result = when (operator) {
+                "+" -> num1 + num2
+                "−" -> num1 - num2
+                "×" -> num1 * num2
+                "÷" -> if (num2 != 0.0) num1 / num2 else Double.NaN
+                else -> 0.0
+            }
+            clearAll()
+            val resultString =
+                if (result.isNaN())
+                    "Error"
+                else
+                    result.toString().removeSuffix(".0")
+            number1 =
+                if (result.isNaN())
+                    ""
+                else
+                    resultString
+            state = state.copy(resultString)
+        }
     }
 }
